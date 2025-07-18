e17847a977fdbda0d3f0e043f83af878d4021d3e
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-07-18 13:37:16 -0400

    feat: setup sample server

---
+++ sample_server.rb
@@ -0,0 +1,38 @@
+# CHQ: Gemini AI generated the template server
+# Import the WEBrick library, which is a standard Ruby library for creating web servers.
+require 'webrick'
+
+# Define the port number on which the server will listen.
+# You can change this to any available port, e.g., 3000, 8080.
+PORT = 4026
+
+# Create a new WEBrick HTTP server instance.
+# The :Port option specifies the port number.
+server = WEBrick::HTTPServer.new(:Port => PORT)
+
+# Register a handler for the root path ("/").
+# When a request comes in for this path, the block will be executed.
+# req: The HTTP request object, containing details about the client's request.
+# res: The HTTP response object, which you will populate with the server's response.
+server.mount_proc '/' do |req, res|
+  # Set the content type of the response to HTML.
+  # This tells the browser how to interpret the response body.
+  res.content_type = 'text/html'
+
+  # Set the body of the response. This is the content that will be sent back to the client.
+  res.body = '<h1>Hello, Ruby Server!</h1><p>This is a simple server built with WEBrick.</p>'
+
+  # You can also inspect request details if needed:
+  # puts "Request Path: #{req.path}"
+  # puts "Request Method: #{req.request_method}"
+end
+
+# Register a signal trap for INT (interrupt) signal (Ctrl+C).
+# When Ctrl+C is pressed, this block will be executed, gracefully shutting down the server.
+trap 'INT' do
+  server.shutdown
+end
+
+# Start the server. This will make the server listen for incoming requests.
+puts "Starting Ruby server on http://localhost:#{PORT}/"
+server.start
